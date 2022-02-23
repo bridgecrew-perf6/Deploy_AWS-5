9076ff99b0e1e3777d1ce49003c1bbb1007b065a
parkjeyoung <dkfmwpsxm@naver.com>
2022-02-23 16:34:25 +0900

    fix : sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,3 +7,6 @@ export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
+
+sleep 10s && pm2 status // pm2동작을 확인 - 제대로 작동했는지, codedeploy ->로그 확인
+echo $DATABASE_HOST     // 환경변수에 DATABASE_HOST의 값이 무엇인지 확인
